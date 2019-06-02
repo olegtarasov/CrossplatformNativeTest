b03ececa6ae235aa1011c5e79e5d0e420a8a645f
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-06-02 14:11:25 +0300

    Initial commit

---
+++ CoreNativeTest/CoreNativeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using NativeLibraryManager;
+
+namespace CoreNativeTest
+{
+    internal class Program
+    {
+        [DllImport("TestLib")]
+        private static extern void hello();
+    
+        private static void Main(string[] args)
+        {
+            var accessor = new ResourceAccessor(Assembly.GetExecutingAssembly());
+            var libManager = new LibraryManager(
+                Assembly.GetExecutingAssembly(),
+                new LibraryItem(Platform.MacOs, Bitness.x64,
+                    new LibraryFile("libTestLib.dylib", accessor.Binary("libTestLib.dylib"))));
+            
+            libManager.LoadNativeLibrary();
+            
+            hello();
+            Console.WriteLine("Hello from C#!");
+        }
+    }
+}
---
+++ TestLib/library.h
@@ -0,0 +1,14 @@
+#ifndef TESTLIB_LIBRARY_H
+#define TESTLIB_LIBRARY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hello();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //TESTLIB_LIBRARY_H
