03eaa3a51578557a82e63e5961f1e28d83c61a89
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-06-07 21:57:03 +0300

    Added Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Building a cross-platform C++ library to call from .NET Core
+
+This repository contains source code for a series of blog posts. Use tags to view snapshots corresponding to articles:
+
+1. https://medium.com/@oleg.tarasov/building-a-cross-platform-c-library-to-call-from-net-core-6fa90f3f1d5d — `1_hello_world`.
--- TestLib/library.h
+++ TestLib/library.h
@@ -2,17 +2,17 @@
 #define TESTLIB_LIBRARY_H
 
 #if defined DLL_EXPORTS
-	#if defined WIN32
-		#define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
-	#else
-		#define LIB_API(RetType) extern "C" RetType __attribute__((visibility("default")))
-	#endif
+    #if defined WIN32
+        #define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
+    #else
+        #define LIB_API(RetType) extern "C" RetType __attribute__((visibility("default")))
+    #endif
 #else
-	#if defined WIN32
-		#define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
-	#else
-		#define LIB_API(RetType) extern "C" RetType
-	#endif
+    #if defined WIN32
+        #define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
+    #else
+        #define LIB_API(RetType) extern "C" RetType
+    #endif
 #endif
 
 LIB_API(void) hello();
